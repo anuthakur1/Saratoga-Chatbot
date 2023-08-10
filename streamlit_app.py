fae9d699d2da62dbe372de1055a1b47fd26e1d88
Anu <anuthakur2006@gmail.com>
2023-08-09 19:28:45 -0700

    combined with crawler

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,93 @@ from langchain.vectorstores import FAISS
 import pytesseract
 import openai
 import os
+import logging
+from urllib.parse import urljoin
+import requests
+from bs4 import BeautifulSoup
+import re
+import langchain
+from langchain.document_loaders import WebBaseLoader
+
+logging.basicConfig(
+    format='%(asctime)s %(levelname)s:%(message)s',
+    level=logging.INFO)
+
+class Crawler:
+    
+    def get_domain(self, url):
+        """Gets the domain of a URL.
+
+        Args:
+            url (str): The URL.
+
+        Returns:
+            str: The domain of the URL.
+        """
+
+        match = re.match(r"https?://(.[^/:]+)(:\d+)?/?.*", url)
+        if match:
+            return match.group(1)
+        else:
+            return None
+    
+    def is_same_domain(self, url1, url2):
+        """Checks if two URLs have the same domain.
+
+        Args:
+          url1 (str): The first URL.
+         url2 (str): The second URL.
+
+        Returns:
+          bool: True if the two URLs have the same domain, False otherwise.
+        """
+
+        domain1 = self.get_domain(url1)
+        domain2 = self.get_domain(url2)
+
+        return domain1 == domain2
+
+    def __init__(self, urls=[]):
+        self.visited_urls = []
+        self.urls_to_visit = urls
+        self.count = 0
+
+    def download_url(self, url):
+        print("attempting to download " + url)
+        return requests.get(url).text
+
+    def get_linked_urls(self, url, html):
+        soup = BeautifulSoup(html, 'html.parser')
+        for link in soup.find_all('a'):
+            path = link.get('href')
+            if path and path.startswith('/'):
+                path = urljoin(url, path)
+            yield path
+            
+    def add_url_to_visit(self, url):
+        if url not in self.visited_urls and url not in self.urls_to_visit:
+            self.urls_to_visit.append(url)
+
+    def crawl(self, url):
+        print("crawling url " + url)
+        html = self.download_url(url)
+        #print(html)
+        for url1 in self.get_linked_urls(url, html):
+            if (url1 and self.is_same_domain(url, url1)):
+                print("queuing up " + url1)
+                self.add_url_to_visit(url1)
+
+    def run(self):
+        while self.urls_to_visit and self.count <= 10:
+            self.count = self.count+1
+            url = self.urls_to_visit.pop(0)
+            logging.info(f'Crawling: {url}')
+            try:
+                self.crawl(url)
+            except Exception:
+                logging.exception(f'Failed to crawl: {url}')
+            finally:
+                self.visited_urls.append(url)
 
 #os.environ["openai_secret_key"] == st.secrets["openai_secret_key"]
 
