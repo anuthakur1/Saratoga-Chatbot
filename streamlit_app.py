98f9cc0f5678dc03ab9ce0ad6d9994eb29dda419
shashidharthakur <38197239+shashidharthakur@users.noreply.github.com>
2023-08-16 13:11:18 -0700

    Use st.cache_resource to ensure that expensive resources are created only once.

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,6 @@ from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
 
 class AppHelper(object):
-    initialized = False
     def __new__(cls):
         """ creates a singleton object, if it is not created,
         or else returns the previous singleton object"""
@@ -26,43 +25,42 @@ class AppHelper(object):
             cls.instance = super(AppHelper, cls).__new__(cls)
 
         return cls.instance
+
+    @st.cache_resource
+    def init_resources(self):
+      # Download OpenAI embeddings and map document chunks to embeddings in vector db
+      embeddings = OpenAIEmbeddings()
+      self.db = FAISS.load_local("faiss_index", embeddings)
+      print("Loaded db from file ")
+      print(self.initialized)
+      #os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
+      self.llm = OpenAI(temperature=0)
+      
+      question_prompt_template1 = """Use the following portion of a long document to see if any of the text is relevant to answer the question.
+      Return any relevant text in English.
+      {text}
+      Question: {question}
+      Relevant text, if any, in English:"""
+      self.QUESTION_PROMPT1 = PromptTemplate(
+      template=question_prompt_template1, input_variables=["text", "question"]
+      )
+      
+      combine_prompt_template1 = """Given the following extracted parts of a long document and a question, create a 1000 word essay with references ("SOURCES") for each sentence.
+      If you don't know the answer, just say that you don't know. Don't try to make up an answer.
+      ALWAYS return a "SOURCES" part in your answer.
+      Respond in English.
+      
+      QUESTION: {question}
+      =========
+      {text}
+      =========
+      FINAL ANSWER IN English:"""
+      self.COMBINE_PROMPT1 = PromptTemplate(
+      template=combine_prompt_template1, input_variables=["text", "question"]
+      )
     
     def __init__(self):
-        if (not self.initialized):
-          # Download OpenAI embeddings and map document chunks to embeddings in vector db
-          embeddings = OpenAIEmbeddings()
-          self.db = FAISS.load_local("faiss_index", embeddings)
-          print("Loaded db from file ")
-          print(self.initialized)
-          #os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
-          self.llm = OpenAI(temperature=0)
-          
-          question_prompt_template1 = """Use the following portion of a long document to see if any of the text is relevant to answer the question.
-          Return any relevant text in English.
-          {text}
-          Question: {question}
-          Relevant text, if any, in English:"""
-          self.QUESTION_PROMPT1 = PromptTemplate(
-          template=question_prompt_template1, input_variables=["text", "question"]
-          )
-          
-          combine_prompt_template1 = """Given the following extracted parts of a long document and a question, create a 1000 word essay with references ("SOURCES") for each sentence.
-          If you don't know the answer, just say that you don't know. Don't try to make up an answer.
-          ALWAYS return a "SOURCES" part in your answer.
-          Respond in English.
-          
-          QUESTION: {question}
-          =========
-          {text}
-          =========
-          FINAL ANSWER IN English:"""
-          self.COMBINE_PROMPT1 = PromptTemplate(
-          template=combine_prompt_template1, input_variables=["text", "question"]
-          )
-          self.initialized = True
-          print(self.initialized)
-
-helper = AppHelper()
+      self.init_resources()
 
 # We will get the user's input by calling the get_text function
 def get_text():
@@ -75,6 +73,8 @@ def generate_response(query, db):
   answer = chain.run(input_documents=docs, question=query)
   answer.replace ('. ', '.\n')
   return answer
+
+helper = AppHelper()
 
 import streamlit as st
 from streamlit_chat import message
