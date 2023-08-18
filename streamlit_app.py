5f0362b2a71190b56eed31adc7a3bb9f6734e66b
Anu <anuthakur2006@gmail.com>
2023-08-17 21:34:14 -0700

    working

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,64 +17,53 @@ from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
 
-class AppHelper(object):
-    def __new__(cls):
-        """ creates a singleton object, if it is not created,
-        or else returns the previous singleton object"""
-        if not hasattr(cls, 'instance'):
-            cls.instance = super(AppHelper, cls).__new__(cls)
+@st.cache_resource
+def init_resources():
+  # Download OpenAI embeddings and map document chunks to embeddings in vector db
+  embeddings = OpenAIEmbeddings()
+  db = FAISS.load_local("faiss_index", embeddings)
+  print("Loaded db from file ")
+  #os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
+  llm = OpenAI(temperature=0)
+  
+  question_prompt_template = """Use the following portion of a long document to see if any of the text is relevant to answer the question.
+  Return any relevant text in English.
+  {text}
+  Question: {question}
+  Relevant text, if any, in English:"""
+  QUESTION_PROMPT = PromptTemplate(
+  template=question_prompt_template, input_variables=["text", "question"]
+  )
+  
+  combine_prompt_template = """Given the following extracted parts of a long document and a question, create a 1000 word essay with references ("SOURCES") for each sentence.
+  If you don't know the answer, just say that you don't know. Don't try to make up an answer.
+  ALWAYS return a "SOURCES" part in your answer.
+  Respond in English.
+  
+  QUESTION: {question}
+  =========
+  {text}
+  =========
+  FINAL ANSWER IN English:"""
+  COMBINE_PROMPT = PromptTemplate(
+  template=combine_prompt_template, input_variables=["text", "question"]
+  )
 
-        return cls.instance
-
-    @st.cache_resource
-    def init_resources(self):
-      # Download OpenAI embeddings and map document chunks to embeddings in vector db
-      embeddings = OpenAIEmbeddings()
-      self.db = FAISS.load_local("faiss_index", embeddings)
-      print("Loaded db from file ")
-      print(self.initialized)
-      #os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
-      self.llm = OpenAI(temperature=0)
-      
-      question_prompt_template1 = """Use the following portion of a long document to see if any of the text is relevant to answer the question.
-      Return any relevant text in English.
-      {text}
-      Question: {question}
-      Relevant text, if any, in English:"""
-      self.QUESTION_PROMPT1 = PromptTemplate(
-      template=question_prompt_template1, input_variables=["text", "question"]
-      )
-      
-      combine_prompt_template1 = """Given the following extracted parts of a long document and a question, create a 1000 word essay with references ("SOURCES") for each sentence.
-      If you don't know the answer, just say that you don't know. Don't try to make up an answer.
-      ALWAYS return a "SOURCES" part in your answer.
-      Respond in English.
-      
-      QUESTION: {question}
-      =========
-      {text}
-      =========
-      FINAL ANSWER IN English:"""
-      self.COMBINE_PROMPT1 = PromptTemplate(
-      template=combine_prompt_template1, input_variables=["text", "question"]
-      )
-    
-    def __init__(self):
-      self.init_resources()
+  return (db, llm, QUESTION_PROMPT, COMBINE_PROMPT)
 
 # We will get the user's input by calling the get_text function
 def get_text():
   input_text = st.text_input("You: ", "Hello, how are you?", key="input")
   return input_text
 
-def generate_response(query, db):
+def generate_response(query, db, llm, QUESTION_PROMPT, COMBINE_PROMPT):
   docs = db.similarity_search(query, k=20)
-  chain = load_summarize_chain(helper.llm, chain_type="map_reduce", map_prompt=helper.QUESTION_PROMPT1, combine_prompt=helper.COMBINE_PROMPT1)
+  chain = load_summarize_chain(llm, chain_type="map_reduce", map_prompt=QUESTION_PROMPT, combine_prompt=COMBINE_PROMPT)
   answer = chain.run(input_documents=docs, question=query)
   answer.replace ('. ', '.\n')
   return answer
 
-helper = AppHelper()
+(db, llm, QUESTION_PROMPT, COMBINE_PROMPT) = init_resources()
 
 import streamlit as st
 from streamlit_chat import message
@@ -93,7 +82,7 @@ user_input = get_text()
 if user_input:
   #output = generate_response(user_input)
   # store the output
-  output = generate_response(user_input, helper.db)
+  output = generate_response(user_input, db, llm, QUESTION_PROMPT, COMBINE_PROMPT)
   st.session_state.past.append(user_input)
   st.session_state.generated.append(output)
 
